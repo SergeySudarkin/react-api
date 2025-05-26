3d79635f6f9b4b2902792e17595a333dfa22b0a9
Sergey Sudarkin <s.sudarkin@mail.ru>
2025-05-26 17:35:55 +0300

    delayFn, useFetch

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -2,27 +2,30 @@ import { useState, useEffect } from "react";
 import { API_URL } from "../../constants";
 import cls from "./HomePage.module.css";
 import { PreviewCardList } from "../../components/PreviewCardList";
+import { Loader } from "../../components/Loader";
+import { useFetch } from "../../hooks/useFetch";
 
 export const HomePage = () => {
     const [cards, setCards] = useState([]);
 
-    const getCard = async () => {
-        try {
-            const response = await fetch(API_URL);
-            const cardsObject = await response.json();
-            const cards = cardsObject.items;
-            setCards(cards);
-        } catch (error) {
-            console.error(error.message);
-        }
-    };
+    const [getCards, isLoading, error] = useFetch(async () => {
+        const response = await fetch(API_URL);
+        const cardsObject = await response.json();
+        const cards = cardsObject.items;
+
+        setCards(cards);
+        return cards;
+    });
 
     useEffect(() => {
-        getCard();
+        getCards();
     }, []);
 
     return (
         <>
+            {isLoading && <Loader />}
+            {error && <p>{error}</p>}
+
             <h1 className={cls.homeTitle}>Новомученики и Исповедники</h1>
             <PreviewCardList cards={cards} />
         </>
